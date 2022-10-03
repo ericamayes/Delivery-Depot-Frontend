12f78aaa8fc9255be7d19fb2d18ede0070e2bfdb
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-03 13:15:13 -0600

    edward

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+function Contact (){
+    return(
+        <div>
+            <h1>Contact us!</h1>
+            <p>Contact our customer service for problems with orders via email or phone</p>
+            <ul>
+                <li> Phone: +1 (145)-353-0004</li>
+                <li> Email: delivery.depot@gmail.com</li>
+            </ul>
+
+        </div>
+    )
+}
