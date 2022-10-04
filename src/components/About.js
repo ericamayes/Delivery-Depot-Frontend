8cdbbd458592078d39dec39415b2c63add71c733
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-04 11:10:34 -0600

    about and contact

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,39 @@
 import React from "react"; 
 
-function Home () {
+function About () {
+    return(
+        <div>
+            <span role="img" classname="logo-head">
+                <img className="logo" src="https://png.pngitem.com/pimgs/s/208-2089295_transparent-moving-png-images-transportation-transparent-background-png.png"></img>
+            <h1>Delivery Depot</h1>
+              </span>
+        <h1 classname="head"> About Us</h1>
+            <p>We have created a database for food delivery that allows a user on the front end to send a request for a dish to a given restaurant and delivery service.</p>
+        <div className="about">
+            <div className="about-card">
+                <p classname="about-name"> Erica Hashert</p>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/erica-hashert-251a0724b/"> Linkedin</a></li>
+                    <li><a href="https://github.com/ericahashert">Github</a></li>
+                </ul>
+            </div>
+            <div className="about-card">
+                <p classname="about-name">Konstantin Stanmeyer</p>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/edwarddickey/"> Linkedin</a> </li>
+                    <li><a href="https://github.com/edwarddickey?tab=repositories">Github</a></li>
+                </ul>
+            </div>
+            <div className="about-card">
+                <p classname="about-name">Edward Dickey</p>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/konstantin-stanmeyer-b06b88185/"> Linkedin</a></li>
+                    <li><a href="https://github.com/konstantinstanmeyer">Github</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+    )
 }
 
-export default Home;
+export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,16 @@
 import React from "react"; 
 
-function Contact () {
-}
+function Contact (){
+    return(
+        <div>
+            <h1 classname="head">Contact us!</h1>
+            <p>Contact our customer service for problems with orders via email or phone</p>
+            <ul>
+                <li> Phone: +1 (145)-353-0004</li>
+                <li> Email: delivery.depot@gmail.com</li>
+            </ul>
 
+        </div>
+    )
+}
 export default Contact;
