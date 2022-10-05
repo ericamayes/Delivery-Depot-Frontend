b9ebfbe28d70621de3366cbcd5de953c5e8d2f6c
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-05 14:00:23 -0600

    fixed about and contact

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,32 @@
 import React from "react"; 
+import NavBar from "./NavBar";
 
 function About () {
     return(
         <div>
-            <span role="img" classname="logo-head">
-                <img className="logo" src="https://png.pngitem.com/pimgs/s/208-2089295_transparent-moving-png-images-transportation-transparent-background-png.png"></img>
-            <h1>Delivery Depot</h1>
-              </span>
-        <h1 classname="head"> About Us</h1>
-            <p>We have created a database for food delivery that allows a user on the front end to send a request for a dish to a given restaurant and delivery service.</p>
-        <div className="about">
+            <NavBar/>
+        <h1 className="head"> About Us</h1>
+            <p className = "about-bio" >We have created a database for food delivery that allows a user on the front end to send a request for a dish to a given restaurant and delivery service.</p>
+        <div id="about">
             <div className="about-card">
-                <p classname="about-name"> Erica Hashert</p>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/erica-hashert-251a0724b/"> Linkedin</a></li>
-                    <li><a href="https://github.com/ericahashert">Github</a></li>
+                <p className="about-name"> Erica Hashert</p>
+                <ul id = "icons0">
+                    <li><a href="https://www.linkedin.com/in/erica-hashert-251a0724b/"><img className="icon"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBMtuIUhxhfXNKoDCA2gX_zvPtAiB9M__czdTgiZKODw&s"/></a></li>
+                    <li><a href="https://github.com/ericahashert"><img className="icon"src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/></a></li>
                 </ul>
             </div>
             <div className="about-card">
-                <p classname="about-name">Konstantin Stanmeyer</p>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/edwarddickey/"> Linkedin</a> </li>
-                    <li><a href="https://github.com/edwarddickey?tab=repositories">Github</a></li>
+                <p className="about-name">Konstantin Stanmeyer</p>
+                <ul id = "icons1">
+                    <li><a href="https://www.linkedin.com/in/edwarddickey/"><img className="icon"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBMtuIUhxhfXNKoDCA2gX_zvPtAiB9M__czdTgiZKODw&s"/></a> </li>
+                    <li><a href="https://github.com/edwarddickey?tab=repositories"><img className="icon"src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/></a></li>
                 </ul>
             </div>
             <div className="about-card">
-                <p classname="about-name">Edward Dickey</p>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/konstantin-stanmeyer-b06b88185/"> Linkedin</a></li>
-                    <li><a href="https://github.com/konstantinstanmeyer">Github</a></li>
+                <p className="about-name">Edward Dickey</p>
+                <ul id = "icons2">
+                    <li><a href="https://www.linkedin.com/in/konstantin-stanmeyer-b06b88185/"><img className="icon"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBMtuIUhxhfXNKoDCA2gX_zvPtAiB9M__czdTgiZKODw&s"/></a></li>
+                    <li><a href="https://github.com/konstantinstanmeyer"><img className="icon"id = "img2" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/></a></li>
                 </ul>
             </div>
         </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from "react"; 
+import NavBar from "./NavBar";
 
 function Contact (){
     return(
         <div>
-            <h1 classname="head">Contact us!</h1>
-            <p>Contact our customer service for problems with orders via email or phone</p>
-            <ul>
+            <NavBar/>
+            <h1 className="head">Contact us!</h1>
+            <p className = "about-bio">Contact our customer service for problems with orders via email or phone</p>
+            <ul className ="contact">
                 <li> Phone: +1 (145)-353-0004</li>
                 <li> Email: delivery.depot@gmail.com</li>
             </ul>
