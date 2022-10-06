c1a9b8f4d37777b8514596f5024af1fd04cd403c
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-06 10:58:17 -0600

    dishes1

--- src/components/DishesPage.js
+++ src/components/DishesPage.js
@@ -11,41 +11,17 @@ function DishPage () {
     const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
-        fetch(`http://localhost:9292/delivery/${window.location.href.slice(-1)}/restaurants`)
+        fetch(`http://localhost:9292/restaurants/${window.location.href.charAt(-7)}/dishes`)
         .then((r) => r.json())
       .then((restaurant) => setRestaurant(restaurant));
   }, []);
+console.log(window.location.href.substring(-7))
 
-  useEffect(() => {
-    console.log(restaurant)
-  }, [restaurant])
 
-  useEffect(() => {
-    let id = window.location.href.slice(-1)
-    switch (id) {
-        case '1': 
-            setHeader("Grubhub");
-            break;
-        case '2':
-            setHeader("Postmates");
-            break;
-        case '3':
-            setHeader("Doordash");
-            break;
-        case '4':
-            setHeader("Uber Eats");
-            break;
-        case '5':
-            setHeader("JoyRun");
-            break;
-        default:
-            setHeader("Invalid ID");
-    }
-  }, [])
 
   if (!restaurant) return <h2>Loading available restaurants...</h2>;
 
-  const displayedRestaurants = restaurant.filter((restaurant) => {
+  const displayedDishes = restaurant.filter((restaurant) => {
     return restaurant.category.toLowerCase().includes(searchTerm.toLowerCase());
   })
 
@@ -60,7 +36,7 @@ function DishPage () {
           <h1 className="restaurant-header">Uber Eats</h1>
         </div>
         <RestaurantSearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
-        <DishList restaurants={displayedRestaurants}/>
+        <DishList restaurants={displayedDishes}/>
       </div>
       <div className="order-column">
         <h3>My Order:</h3>
