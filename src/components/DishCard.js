b5b6bae50aeee7ced136313873238543911c6bad
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-06 15:43:28 -0600

    starting handle click

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,12 +1,18 @@
 function DishCard( {restaurant} ) {
-  const {name, category, image_url} = restaurant;
+  const {name, ingredients, vegetarian, image_url} = restaurant;
+
+  function handleClick(){
+    
+  }
+  
 //   should we also do rating? can't access it from the current table, though
 
   return (
     <li className="individual-card">
       <img src={image_url} alt={name} />
       <h4>{name}</h4>
-      <p>Category: {category}</p>
+      <p>Ingredients: {ingredients}</p>
+      <p>Vegitarian?: {vegetarian}</p>
       <btn className = "order-btn">
         Add to order
       </btn>
